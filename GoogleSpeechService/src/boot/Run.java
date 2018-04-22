503705f4277b5d4de3154393c982f0c8a012eb6b
galezra <galezra@gmail.com>
2018-04-22 23:12:37 +0300

    "Run" class to test the service

--- GoogleSpeechService/src/boot/Run.java
+++ GoogleSpeechService/src/boot/Run.java
@@ -1,61 +1,59 @@
 package boot;
-
-import com.google.api.gax.core.FixedCredentialsProvider;
-import com.google.auth.oauth2.GoogleCredentials;
-//Imports the Google Cloud client library
-import com.google.cloud.speech.v1.RecognitionAudio;
-import com.google.cloud.speech.v1.RecognitionConfig;
-import com.google.cloud.speech.v1.RecognitionConfig.AudioEncoding;
-import com.google.cloud.speech.v1.RecognizeResponse;
-import com.google.cloud.speech.v1.SpeechClient;
-import com.google.cloud.speech.v1.SpeechRecognitionAlternative;
-import com.google.cloud.speech.v1.SpeechRecognitionResult;
-import com.google.cloud.speech.v1.SpeechSettings;
-import com.google.protobuf.ByteString;
-
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
+import java.util.ArrayList;
+
 
 public class Run {
 	public static void main(String[] args) throws Exception, IOException {
 		
-		// Configure GoogleCredential
-		FileInputStream credentialsStream = new FileInputStream("My Project-d3bff28438ec.json");
-		GoogleCredentials credentials = GoogleCredentials.fromStream(credentialsStream);
-		FixedCredentialsProvider credentialsProvider = FixedCredentialsProvider.create(credentials);
-
-		SpeechSettings speechSettings = 
-				SpeechSettings.newBuilder()
-					.setCredentialsProvider(credentialsProvider)
-					.build();
-
-		SpeechClient speech = SpeechClient.create(speechSettings);
+		SpeechToText st = new SpeechToText();
 		
-		// Initial audio to translate
 		Path path = Paths.get("./resources/fix.wav");
 		byte[] data = Files.readAllBytes(path);
-		ByteString audioBytes = ByteString.copyFrom(data);
+		ArrayList<String> test = new ArrayList<String>();
+		test = st.getConvertText(data);
+		System.out.println(test.toString());
 
-		// Configure request with local raw PCM audio
-		RecognitionConfig config = RecognitionConfig.newBuilder().setEncoding(AudioEncoding.ENCODING_UNSPECIFIED)
-				.setLanguageCode("he-il").setSampleRateHertz(16000).build();
-		RecognitionAudio audio = RecognitionAudio.newBuilder().setContent(audioBytes).build();
-
-		// Use blocking call to get audio transcript
-		RecognizeResponse response = speech.recognize(config, audio);
-		List<SpeechRecognitionResult> results = response.getResultsList();
-
-		for (SpeechRecognitionResult result : results) {
-			// There can be several alternative transcripts for a given chunk of
-			// speech. Just use the
-			// first (most likely) one here.
-			SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
-			System.out.printf("Transcription: %s%n", alternative.getTranscript());
-		}
-		speech.close();
 	}
 }
+
+
+/*
+// Configure GoogleCredential
+FileInputStream credentialsStream = new FileInputStream("My First Project-fa257743d788.json");
+GoogleCredentials credentials = GoogleCredentials.fromStream(credentialsStream);
+FixedCredentialsProvider credentialsProvider = FixedCredentialsProvider.create(credentials);
+
+SpeechSettings speechSettings = 
+		SpeechSettings.newBuilder()
+			.setCredentialsProvider(credentialsProvider)
+			.build();
+
+SpeechClient speech = SpeechClient.create(speechSettings);
+
+// Initial audio to translate
+Path path = Paths.get("./resources/fix.wav");
+byte[] data = Files.readAllBytes(path);
+ByteString audioBytes = ByteString.copyFrom(data);
+
+// Configure request with local raw PCM audio
+RecognitionConfig config = RecognitionConfig.newBuilder().setEncoding(AudioEncoding.ENCODING_UNSPECIFIED)
+		.setLanguageCode("he-il").setSampleRateHertz(16000).build();
+RecognitionAudio audio = RecognitionAudio.newBuilder().setContent(audioBytes).build();
+
+// Use blocking call to get audio transcript
+RecognizeResponse response = speech.recognize(config, audio);
+List<SpeechRecognitionResult> results = response.getResultsList();
+
+for (SpeechRecognitionResult result : results) {
+	// There can be several alternative transcripts for a given chunk of
+	// speech. Just use the
+	// first (most likely) one here.
+	SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
+	System.out.printf("Transcription: %s%n", alternative.getTranscript());
+}
+speech.close();
+*/
